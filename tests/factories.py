9103c367b4e7aaac2e153c77b81438b80fa226a4
Greg Reinbach <greg@reinbach.com>
2018-04-26 14:25:03 -0400

    Add backend tests

--- tests/factories.py
+++ tests/factories.py
@@ -1,20 +1,46 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 import factory
 
 from factory import fuzzy
+
 from demo.example.models import Organization
+from realm import models
 
 
-class UserFactory(factory.Factory):
+class UserFactory(factory.django.DjangoModelFactory):
+    username = fuzzy.FuzzyText(length=100)
+
     class Meta:
-        model = User
-
-    username = 'test'
+        model = get_user_model()
 
 
-class OrganizationFactory(factory.Factory):
+class OrganizationFactory(factory.django.DjangoModelFactory):
+    name = fuzzy.FuzzyText(length=100)
+
     class Meta:
         model = Organization
 
-    name = fuzzy.FuzzyText(length=100)
+
+class RealmFactory(factory.django.DjangoModelFactory):
+    user = factory.SubFactory(UserFactory)
+    organization = factory.SubFactory(OrganizationFactory)
+
+    class Meta:
+        model = models.Realm
+
+
+class GroupFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Group
+
+
+class PermissionFactory(factory.django.DjangoModelFactory):
+    name = fuzzy.FuzzyText()
+    content_type = ContentType.objects.first()
+    codename = fuzzy.FuzzyText()
+
+    class Meta:
+        model = Permission
---
+++ tests/test_backend.py
@@ -0,0 +1,177 @@
+from django.contrib.auth.models import Permission
+
+from realm.backends import RealmBackend
+from tests.base import BaseTestCase
+from tests.factories import (
+    GroupFactory,
+    PermissionFactory,
+    RealmFactory,
+    UserFactory,
+)
+
+
+class TestRealmBackend(BaseTestCase):
+    def setUp(self):
+        self.permission = PermissionFactory()
+        self.permission_app_label = self.permission.content_type.app_label
+        self.permission_label = "{}.{}".format(
+            self.permission_app_label,
+            self.permission.codename
+        )
+        self.backend = RealmBackend()
+
+    def test_internal_get_realm_permissions_empty(self):
+        realm = RealmFactory(workspace=self.tenant)
+        self.assertEqual(len(self.backend._get_realm_permissions(realm)), 0)
+
+    def test_internal_get_realm_permissions(self):
+        realm = RealmFactory(workspace=self.tenant)
+        realm.realm_permissions.add(self.permission)
+        perms = self.backend._get_realm_permissions(realm)
+        self.assertEqual(len(perms), 1)
+        self.assertIn(self.permission, perms)
+
+    def test_internal_get_group_permissions_empty(self):
+        realm = RealmFactory(workspace=self.tenant)
+        self.assertEqual(len(self.backend._get_group_permissions(realm)), 0)
+
+    def test_internal_get_group_permissions(self):
+        group = GroupFactory()
+        group.permissions.add(self.permission)
+        realm = RealmFactory(workspace=self.tenant)
+        realm.groups.add(group)
+        perms = self.backend._get_group_permissions(realm)
+        self.assertEqual(len(perms), 1)
+        self.assertIn(self.permission, perms)
+
+    def test_internal_get_permission_user_not_active(self):
+        user = UserFactory(is_active=False)
+        realm = RealmFactory(user=user, workspace=self.tenant)
+        perms = self.backend._get_permissions(realm, None, "realm")
+        self.assertEqual(perms, set())
+
+    def test_internal_get_permission_obj_not_none(self):
+        realm = RealmFactory(workspace=self.tenant)
+        perms = self.backend._get_permissions(realm, realm, "realm")
+        self.assertEqual(perms, set())
+
+    def test_internal_get_permission_superuser(self):
+        user = UserFactory(is_superuser=True)
+        realm = RealmFactory(user=user, workspace=self.tenant)
+        perms = self.backend._get_permissions(realm, None, "realm")
+        self.assertEqual(len(perms), Permission.objects.count())
+
+    def test_internal_get_permissions_realm_empty(self):
+        realm = RealmFactory(workspace=self.tenant)
+        perms = self.backend._get_permissions(realm, None, "realm")
+        self.assertEqual(len(perms), 0)
+
+    def test_internal_get_permissions_realm(self):
+        realm = RealmFactory(workspace=self.tenant)
+        realm.realm_permissions.add(self.permission)
+        perms = self.backend._get_permissions(realm, None, "realm")
+        self.assertEqual(len(perms), 1)
+        self.assertIn(self.permission_label, perms)
+
+    def test_internal_get_permissions_group_empty(self):
+        realm = RealmFactory(workspace=self.tenant)
+        perms = self.backend._get_permissions(realm, None, "group")
+        self.assertEqual(len(perms), 0)
+
+    def test_internal_get_permissions_group(self):
+        group = GroupFactory()
+        group.permissions.add(self.permission)
+        realm = RealmFactory(workspace=self.tenant)
+        realm.groups.add(group)
+        perms = self.backend._get_permissions(realm, None, "group")
+        self.assertEqual(len(perms), 1)
+        self.assertIn(self.permission_label, perms)
+
+    def test_get_realm_permissions_empty(self):
+        realm = RealmFactory(workspace=self.tenant)
+        perms = self.backend.get_realm_permissions(realm, None)
+        self.assertEqual(len(perms), 0)
+
+    def test_get_permissions_realm(self):
+        realm = RealmFactory(workspace=self.tenant)
+        realm.realm_permissions.add(self.permission)
+        perms = self.backend.get_realm_permissions(realm, None)
+        self.assertEqual(len(perms), 1)
+        self.assertIn(self.permission_label, perms)
+
+    def test_get_group_permissions_empty(self):
+        realm = RealmFactory(workspace=self.tenant)
+        perms = self.backend.get_group_permissions(realm, None)
+        self.assertEqual(len(perms), 0)
+
+    def test_get_group_permissions(self):
+        group = GroupFactory()
+        group.permissions.add(self.permission)
+        realm = RealmFactory(workspace=self.tenant)
+        realm.groups.add(group)
+        perms = self.backend.get_group_permissions(realm, None)
+        self.assertEqual(len(perms), 1)
+        self.assertIn(self.permission_label, perms)
+
+    def test_get_all_permissions_empty(self):
+        realm = RealmFactory(workspace=self.tenant)
+        perms = self.backend.get_all_permissions(realm)
+        self.assertEqual(len(perms), 0)
+
+    def test_get_all_permissions_user_not_active(self):
+        user = UserFactory(is_active=False)
+        realm = RealmFactory(user=user, workspace=self.tenant)
+        perms = self.backend.get_all_permissions(realm)
+        self.assertEqual(len(perms), 0)
+
+    def test_get_all_permissions_obj_not_none(self):
+        realm = RealmFactory(workspace=self.tenant)
+        perms = self.backend.get_all_permissions(realm, realm)
+        self.assertEqual(len(perms), 0)
+
+    def test_get_all_permissions(self):
+        group = GroupFactory()
+        group.permissions.add(self.permission)
+        realm = RealmFactory(workspace=self.tenant)
+        realm.groups.add(group)
+        realm.realm_permissions.add(self.permission)
+        perms = self.backend.get_all_permissions(realm)
+        self.assertEqual(len(perms), 1)
+        self.assertIn(self.permission_label, perms)
+
+    def test_has_perm_user_not_active(self):
+        user = UserFactory(is_active=False)
+        realm = RealmFactory(user=user, workspace=self.tenant)
+        self.assertFalse(self.backend.has_perm(realm, self.permission))
+
+    def test_has_perm_false(self):
+        realm = RealmFactory(workspace=self.tenant)
+        self.assertFalse(self.backend.has_perm(realm, self.permission))
+
+    def test_has_perm(self):
+        realm = RealmFactory(workspace=self.tenant)
+        realm.realm_permissions.add(self.permission)
+        self.assertTrue(self.backend.has_perm(realm, self.permission_label))
+
+    def test_has_module_perms_user_not_active(self):
+        user = UserFactory(is_active=False)
+        realm = RealmFactory(user=user, workspace=self.tenant)
+        perm = self.backend.has_module_perms(realm, self.permission_app_label)
+        self.assertFalse(perm)
+
+    def test_has_module_perms_bad_label(self):
+        realm = RealmFactory(workspace=self.tenant)
+        realm.realm_permissions.add(self.permission)
+        perm = self.backend.has_module_perms(realm, "wrong")
+        self.assertFalse(perm)
+
+    def test_has_module_perms_false(self):
+        realm = RealmFactory(workspace=self.tenant)
+        perm = self.backend.has_module_perms(realm, self.permission_app_label)
+        self.assertFalse(perm)
+
+    def test_has_module_perms(self):
+        realm = RealmFactory(workspace=self.tenant)
+        realm.realm_permissions.add(self.permission)
+        perm = self.backend.has_module_perms(realm, self.permission_app_label)
+        self.assertTrue(perm)
