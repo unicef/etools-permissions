192e54cf1476302d932b989343508c655a9be705
Greg Reinbach <greg@reinbach.com>
2018-05-22 12:09:28 -0400

    Update revision in test circleci config script

--- .circleci/test-config.sh
+++ .circleci/test-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user ${CIRCLE_TOKEN}: \
     --request POST \
-    --form revision=532b5ef3a88b6b9bd990371d4404f34b2b9812db \
+    --form revision=f566464b2c856cdba33a8d4e9a85d091529cb1b2 \
     --form config=@config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/unicef/etools-permissions/tree/master
