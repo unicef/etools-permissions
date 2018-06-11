6c038a52f43055c39435f7bd3a1609d951244b2a
Greg Reinbach <greg@reinbach.com>
2018-06-11 13:37:21 -0400

    Update migrate permission to migrate users related to permission and groups

--- src/etools_permissions/management/commands/migrate_permissions.py
+++ src/etools_permissions/management/commands/migrate_permissions.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from django.contrib.auth.models import Group as GroupDjango, Permission as PermissionDjango
 from django.core.management import BaseCommand
 
-from etools_permissions.models import Group, Permission
+from etools_permissions.models import Group, Permission, Realm
 
 
 def get_permission(codename):
@@ -19,7 +19,7 @@ def get_permission_group_map():
     permission_map = defaultdict(list)
     for group in GroupDjango.objects.all():
         for perm in group.permissions.all():
-            permission_map[perm.pk].append(group.name)
+            permission_map[perm.pk].append(group)
     return permission_map
 
 
@@ -46,9 +46,29 @@ def migrate_permissions():
             target=target,
             condition=[],
         )
-        for group_name in permission_map[perm_django.pk]:
-            group = Group.objects.get(name=group_name)
+
+        # users permissions
+        # ignore workspace and organization
+        # as there is no consideration of that in
+        # default django permission/group setup
+        for user in perm_django.user_set.all():
+            realm, _ = Realm.objects.get_or_create(
+                user=user,
+                defaults={
+                    "workspace": None,
+                    "organization": None,
+                }
+            )
+            realm.permissions.add(perm)
+
+        for group_django in permission_map[perm_django.pk]:
+            group = Group.objects.get(name=group_django.name)
             group.permissions.add(perm)
+
+            # group
+            for user in group_django.user_set.all():
+                realm, _ = Realm.objects.get_or_create(user=user)
+                realm.groups.add(group)
 
 
 def migrate_groups():
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import (
 )
 from django.core.management import call_command
 
-from etools_permissions.models import Group, Permission
+from etools_permissions.models import Group, Permission, Realm
+from tests.factories import UserFactory
 from tests.base import BaseTestCase
 
 
@@ -20,6 +21,26 @@ class TestMigratePermissions(BaseTestCase):
         call_command("migrate_permissions")
         self.assertTrue(perm_qs.count() > 1)
         self.assertFalse(group_qs.count())
+
+    def test_command_user(self):
+        django_group_qs = DjangoGroup.objects
+        self.assertFalse(django_group_qs.count())
+        group_qs = Group.objects
+        self.assertFalse(group_qs.count())
+        perm_qs = Permission.objects
+        self.assertFalse(perm_qs.count())
+        user = UserFactory()
+        user.user_permissions.add(DjangoPermission.objects.first())
+        realm_qs = Realm.objects.filter(user=user)
+        self.assertFalse(realm_qs.exists())
+
+        call_command("migrate_permissions")
+        self.assertTrue(perm_qs.count() > 1)
+        self.assertFalse(group_qs.count())
+        self.assertTrue(realm_qs.exists())
+        realm = realm_qs.first()
+        self.assertEqual(realm.permissions.count(), 1)
+        self.assertEqual(realm.groups.count(), 0)
 
     def test_command_group(self):
         django_group_qs = DjangoGroup.objects
@@ -40,3 +61,31 @@ class TestMigratePermissions(BaseTestCase):
         new_group = Group.objects.first()
         self.assertEqual(new_group.name, "Test Group")
         self.assertEqual(new_group.permissions.count(), 1)
+
+    def test_command_group_user(self):
+        django_group_qs = DjangoGroup.objects
+        perm = DjangoPermission.objects.first()
+        group = DjangoGroup.objects.create(
+            name="Test Group",
+        )
+        group.permissions.add(perm)
+        self.assertEqual(django_group_qs.count(), 1)
+        group_qs = Group.objects
+        self.assertFalse(group_qs.count())
+        perm_qs = Permission.objects
+        self.assertFalse(perm_qs.count())
+        user = UserFactory()
+        user.groups.add(group)
+        realm_qs = Realm.objects.filter(user=user)
+        self.assertFalse(realm_qs.exists())
+
+        call_command("migrate_permissions")
+        self.assertTrue(perm_qs.count() > 1)
+        self.assertEqual(group_qs.count(), 1)
+        new_group = Group.objects.first()
+        self.assertEqual(new_group.name, "Test Group")
+        self.assertEqual(new_group.permissions.count(), 1)
+        self.assertTrue(realm_qs.exists())
+        realm = realm_qs.first()
+        self.assertEqual(realm.permissions.count(), 0)
+        self.assertEqual(realm.groups.count(), 1)
